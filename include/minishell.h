1b52a1547d2b3fd6cfa6fe9920e2096ccf739b3a
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-01-20 23:52:02 +0100

    creating structs and the beggining of minishell

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:50:01 by jotrujil          #+#    #+#             */
-/*   Updated: 2025/01/20 18:51:11 by jotrujil         ###   ########.fr       */
+/*   Updated: 2025/01/20 23:30:23 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "../libft/libft.h"
+# include <sys/wait.h>
+
+typedef struct s_prompt
+{
+	t_list	*cmds;
+	pid_t	pid;
+	char	**envp;
+}	t_prompt;
+
+typedef struct	s_mini
+{
+	char	**full_cmd;
+	char	*full_path;
+	int		infile;
+	int		outfile;
+}	t_mini;
+
 
 #endif
