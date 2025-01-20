d814c3fae604b576937bcffc87f47f829a55d546
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-01-20 23:52:02 +0100

    creating structs and the beggining of minishell

--- src/main.c
+++ src/main.c
@@ -3,19 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 12:35:57 by jotrujil          #+#    #+#             */
-/*   Updated: 2025/01/20 20:29:45 by jotrujil         ###   ########.fr       */
+/*   Updated: 2025/01/20 23:39:59 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+int	g_status;
+
+Esta función inicializa el prompt, copia las variables de entorno
+
+t_prompt	ft_start_shell(char **argv, char **envp)
+{
+	
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char	*out;
-
+	t_prompt	prompt;
+	char		*imput;
+	char		*str;
+	
+	prompt = ft_start_shell(argv, envp);
 	while (argc && argv)
 	{
 		out = readline("anon@minishell $ ");
