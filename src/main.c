5903204616b31d5d6b417ff1645daf6a651a488a
Jorge <jorgetrujilloroman@gmail.com>
2025-01-21 11:47:33 +0100

    fixing typos

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
+/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 12:35:57 by jotrujil          #+#    #+#             */
-/*   Updated: 2025/01/20 23:39:59 by davigome         ###   ########.fr       */
+/*   Updated: 2025/01/21 11:46:59 by jotrujil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,31 @@
 
 int	g_status;
 
-Esta función inicializa el prompt, copia las variables de entorno
+//Esta función inicializa el prompt, copia las variables de entorno
 
 t_prompt	ft_start_shell(char **argv, char **envp)
 {
-	
+
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_prompt	prompt;
-	char		*imput;
+	char		*output;
 	char		*str;
-	
+
 	prompt = ft_start_shell(argv, envp);
 	while (argc && argv)
 	{
-		out = readline("anon@minishell $ ");
-		if (out[0] != '\0')
-			add_history(out);
-		if (!out)
+		output = readline("anon@minishell $ ");
+		if (output[0] != '\0')
+			add_history(output);
+		if (!output)
 		{
 			printf("exit\n");
 			return (0);
 		}
-		(void)out;
+		(void)output;
 		(void)envp;
 	}
 }
